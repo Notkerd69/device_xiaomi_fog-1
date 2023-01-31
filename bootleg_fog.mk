92fa33e4d9a8436056adae9504e129eed848495f
Notkeret <jerespn@gmail.com>
2023-01-31 02:08:19 -0400

    correct

--- bootleg_fog.mk
+++ bootleg_fog.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/bootleg/config/common_full_phone.mk)
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := bootleg_fog
